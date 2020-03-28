758d0ee61aa8064f3efa85ec80c345fc3c2e7c81
Duong Do Minh Chau <duongdominhchau@gmail.com>
2020-03-28 17:28:16 +0700

    Shorten output to fit screen

--- examples/color_rgb.cpp
+++ examples/color_rgb.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include <fmt/core.h>
 
-std::string colorExample(int r, int g, int b)
+std::string colorExample(int maxlen, int r, int g, int b)
 {
-    return fmt::format("{:3d},{:3d},{:3d}: {} {}", r, g, b,
+    return fmt::format("{:<{}} {} {}",
+            fmt::format("{},{},{}", r, g, b), maxlen,
             termfmt::format({ termfmt::FgRgb(r, g, b) }, "Text"),
             termfmt::format({ termfmt::BgRgb(r, g, b) }, "Text"));
 }
@@ -13,10 +14,10 @@ int main()
     using namespace termfmt;
     std::cout << "========== RGB color test ==========\n";
     for (int i = 0; i < 256; ++i) {
-        std::cout << colorExample(i, 0, 0) << " "
-                << colorExample(0, i, 0) << " "
-                << colorExample(0, 0, i) << " "
-                << colorExample(i, i / 2, i / 3) << "\n";
+        std::cout << colorExample(7, i, 0, 0) << " "
+                << colorExample(7, 0, i, 0) << " "
+                << colorExample(7, 0, 0, i) << " "
+                << colorExample(10, i, i / 2, i / 3) << "\n";
     }
     return 0;
 }
